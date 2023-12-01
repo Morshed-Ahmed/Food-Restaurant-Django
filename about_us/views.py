f1bca84106ecc27db6aebe41af213daf8efde309
morshedAhmed <morshed50r@gmail.com>
2023-12-01 23:06:37 +0600

    meal and about app added

--- about_us/views.py
+++ about_us/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def about(request):
-    return render(request,'about.html')
+    data = [
+        {
+            "name":"CHRISTOPHER CIPOLLONE",
+            "img":"https://images.getbento.com/accounts/d48c31d33aab2e64c03bffa365760c0f/media/images/2041_S7A0684.JPG?w=1200&fit=max&auto=compress,format"
+        },
+        {
+            "name":"JOHN WINTERMAN",
+            "img":"https://images.getbento.com/accounts/d48c31d33aab2e64c03bffa365760c0f/media/images/44213_S7A0706.JPG?w=1200&fit=max&auto=compress,format"
+        },
+        {
+            "name":"THE FRANCIE TEAM",
+            "img":"https://images.getbento.com/accounts/d48c31d33aab2e64c03bffa365760c0f/media/images/30660IMG_7345.jpg?w=1200&fit=max&auto=compress,format"
+        }
+    ]
+    return render(request,'about.html',{"data":data})
